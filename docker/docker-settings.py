701dab78e0408b32915c31af773540b95f983805
Rafael D. García Galocha <73230083+rafgargal@users.noreply.github.com>
2023-11-02 16:58:53 +0100

    Update docker-settings.py

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD':'',
+        'PASSWORD':'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
